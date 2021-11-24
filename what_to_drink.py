adba7dbf9242ae322ca962fcbbdb438b8943c244
wheruflora <wheruflora@gmail.com>
2021-11-25 00:40:02 +0800

    first code

---
+++ what_to_drink.py
@@ -0,0 +1,7 @@
+import random
+menu = ["Americano","Latte","milk","Heytea","Allcoconut","water",]
+print("Menu:",menu)
+name = input("Your name please: ")
+drink=random.choice(menu)
+print("Hello",name,"!Enjoy your",drink)
+
