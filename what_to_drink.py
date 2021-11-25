f39fefdf57189ad92eaaaf3a2b60e6392927efbd
myleonliu <myleonliu@tencent.com>
2021-11-25 10:27:08 +0800

    add loop

--- what_to_drink.py
+++ what_to_drink.py
@@ -5,7 +5,7 @@ menu = ["Americano","Latte","milk","Heytea","Allcoconut","water",]
 print("Menu:",menu)
 
 while True:
-    name = input("Your name please: ")
+    name = input("Your name please (\'quit\' for quit): ")
 
     if name == 'quit':
         break
